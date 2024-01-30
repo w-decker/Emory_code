bd0f7ffed44a9878151d5e9def6db9ee99893274
Your Name <deckerwill7@gmail.com>
2024-01-30 09:10:26 -0600

    add `plot_univariate()` and `plot_multivariate()`

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # imports
 from utils import *
 from config import IMAGE_DIR
+import matplotlib.pyplot as plt 
 
-# plotting brain animation
+# plot brain animation
 anim1 = anim_random_brain(cmap='YlGnBu', 
                           view='lateral', 
                           func=random_brain,
@@ -12,5 +13,12 @@ anim1 = anim_random_brain(cmap='YlGnBu',
                             fname=f'{IMAGE_DIR}brain1.gif', 
                             save=True)
 
+# plot univariate brain
+uni1 = plot_univariate(rois=[23, 24], view='lateral', cmap='viridis')
+plt.savefig(f'{IMAGE_DIR}brain2.png')
+
+# plot multivariate brain
+multi1 = plot_multivariate(rois=[21, 22, 23, 24], view='lateral', cmap='black_purple_r')
+plt.savefig(f'{IMAGE_DIR}brain3.png')
 
 
--- utils.py
+++ utils.py
@@ -13,25 +13,23 @@ def plot_mwall(cmap):
         
     """
 
-    # get atlas
+    # get brain info
     atlas = datasets.fetch_atlas_surf_destrieux()
-
-    # hemisphere
     hemi = atlas['map_left']
+    fsaverage = datasets.fetch_surf_fsaverage()
 
     # mask
     mask = np.zeros_like(hemi, dtype=float)
     roi_indices = np.where((hemi == 42) | (hemi == 23))
     mask[roi_indices] = np.random.uniform(-10, 10, size=len(roi_indices[0]))
 
-    #fsaverage
-    fsaverage = datasets.fetch_surf_fsaverage()
-
 
     p = plotting.plot_surf_roi(fsaverage['pial_left'], roi_map=mask,
                                 hemi='left', view='medial',
                                 bg_map=fsaverage['sulc_left'], bg_on_data=True,
                                 darkness=.5, cmap=cmap)
+    
+    return p
 
 def random_brain(frames, cmap, view):
     """Generate random brain patterns
@@ -75,3 +73,79 @@ def anim_random_brain(cmap, view, func, frames, fps, interval, fname: str, save:
         anim.save(f'{fname}', writer='imagemagick', fps=fps)
 
     return anim
+
+def plot_univariate(rois, view, cmap: str):
+    """Plot univarate brain and specified regions
+    
+    Parameters
+    ----------
+    rois
+        Regions of interest as specified by destrieux atlas
+
+    view: str
+        Laterial, medial, posterior, ventral
+
+    cmap: str
+        Nilearn color map
+
+    Return
+    --------
+    p: matplotlib object
+    """
+ 
+    # get brain info
+    atlas = datasets.fetch_atlas_surf_destrieux()
+    hemi = atlas['map_left']
+    fsaverage = datasets.fetch_surf_fsaverage()
+
+    # mask
+    n_vertices = len(hemi)
+    mask = np.zeros(n_vertices)
+    for i in rois:
+        mask[hemi == i] = 1
+
+    # plot
+    p = plotting.plot_surf_roi(fsaverage['pial_left'], roi_map=mask,
+                                hemi='left', view='medial',
+                                bg_map=fsaverage['sulc_left'], bg_on_data=True,
+                                darkness=.5, cmap=cmap)
+    
+    return p
+
+def plot_multivariate(rois, view, cmap: str):
+    """Plot multivariate brain and specified regions
+    
+    Parameters
+    ----------
+    rois: list
+        Regions of interest as specified by Destrieux atlas
+
+    view: str
+        Lateral, medial, posterior, ventral
+
+    cmap: str
+        Nilearn color map
+
+    Returns
+    -------
+    p: matplotlib object
+    """
+    
+    # Get brain info
+    atlas = datasets.fetch_atlas_surf_destrieux()
+    hemi = atlas['map_left']
+    fsaverage = datasets.fetch_surf_fsaverage()
+
+    #mask   
+    mask = np.zeros_like(hemi, dtype=float)
+    for i in rois:
+        roi_indices = np.where(hemi == i)
+        mask[roi_indices] = np.random.uniform(0.1, 10, size=len(roi_indices[0]))
+
+    # Plot
+    p = plotting.plot_surf_roi(fsaverage['pial_left'], roi_map=mask,
+                                hemi='left', view=view,
+                                bg_map=fsaverage['sulc_left'], bg_on_data=True,
+                                darkness=0.5, cmap=cmap);
+    
+    return p
